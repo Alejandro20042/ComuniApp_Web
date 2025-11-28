fdf2ab7e9b033b7dc2f820015c18110d87b47c85
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2025-11-28 02:32:18 -0700

    cambios url api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,11 +3,11 @@ import type { RegisterData } from "../interfaces/registerData";
 import api from "./axios";
 
 export const login = async (data: LoginData) => {
-  const res = await api.post("/api/login", data);
+  const res = await api.post("/login", data);
   return res.data;
 };
 
 export const register = async (data: RegisterData) => {
-  const res = await api.post("/api/registro", data);
+  const res = await api.post("/registro", data);
   return res.data;
 };
