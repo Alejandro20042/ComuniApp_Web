aede655ef49874de2870a90522e9beff28a1e5bb
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2025-11-28 02:26:41 -0700

    cambsios axioss

--- src/pages/Home/VoluntarioHome.tsx
+++ src/pages/Home/VoluntarioHome.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
 import type { Solicitud } from "../../interfaces/Solicitud";
 import { useUser } from "../../hooks/useUser";
 import { ChatService } from "../../api/chatService";
