5ce5b200f3b1ac42e6d38e06b3f4d3eb7507d291
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2025-11-28 02:30:59 -0700

    cambios url api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://comuniapp-api-1.onrender.com", 
+  baseURL: "https://comuniapp-api-1.onrender.com/api", 
 });
 
 export default api;
